d49a2994b579cb7d574a1b4a4051546a62e50ff9
Hridoy Hawladar <decentcoderz@gmail.com>
2024-05-04 23:25:15 +0600

    Adding some features

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template lang="">
-  <div class="text-center">hahaha heheheh</div>
+  <div class="flex justify-center items-center h-screen w-screen bg-black">
+    <img class="h-24 w-24 animate-pulse" src="/x-home.jpg" alt="" />
+  </div>
 </template>
-<script setup></script>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,17 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
-export default <Partial<Config>> {
+export default <Partial<Config>>{
   content: [],
   theme: {
-    extend: {
-    }
+    screens: {
+      xsm: "350px",
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+      mx: "1920px",
+    },
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};

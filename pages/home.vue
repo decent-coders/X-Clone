819a6b1a70ed1c49bcb14616cfefd7b45a68d0ea
Hridoy Hawladar <decentcoderz@gmail.com>
2024-05-16 20:05:12 +0600

    Added home page

---
+++ pages/home.vue
@@ -0,0 +1,7 @@
+<template lang="">
+  <div class="flex h-screen justify-center items-center bg-black">
+    <div class="animate-pulse">
+      <h1 class="text-6xl font-bold text-white">Welcome to Home</h1>
+    </div>
+  </div>
+</template>

2f48e357f96bc04dde651d4ef83d75bc42ac115b
Hridoy Hawladar <decentcoderz@gmail.com>
2024-06-20 18:42:12 +0600

    separated following posts

--- components/home/postContainer.vue
+++ components/home/postContainer.vue
@@ -1,14 +1,65 @@
 <template lang="">
   <div id="post_preference" class="w-full border-r border-gray-600 text-white">
     <!-- post option -->
-    <HomePostOption />
+    <div class="border-y border-gray-600 text-white">
+      <div class="flex font-semibold tracking-wide">
+        <!--all post button-->
+        <div
+          class="border-r p-3 border-gray-600 w-1/2 text-center cursor-pointer"
+          @click="ShowAllPosts"
+        >
+          <span
+            class="p-3 w-full font-system"
+            :class="{ 'border-b-4 border-sky-500': activeShowAllPosts }"
+            >For You</span
+          >
+        </div>
+        <!-- ================================= -->
+
+        <!-- following posts button-->
+        <div
+          class="border-r p-3 border-gray-600 w-1/2 text-center cursor-pointer"
+          @click="ShowFollowedPosts"
+        >
+          <span
+            class="p-3 w-full font-system"
+            :class="{
+              'border-b-4 border-sky-500': activeShowFollowedPosts,
+            }"
+            >Following</span
+          >
+        </div>
+        <!-- ================================= -->
+      </div>
+    </div>
     <!-- create post container -->
-    <HomeCreatePostContainer />
+    <HomeCreatePostContainer v-if="activeShowAllPosts" />
     <!-- Posts Container -->
     <div id="post-container">
       <!-- All posts container -->
       <div id="allPosts">
-        <HomePosts> </HomePosts>
+        <HomePosts v-if="activeShowAllPosts"> </HomePosts>
+        <div id="followposts" v-if="activeShowFollowedPosts" class="">
+          <div
+            class="h-[80vh] w-full flex flex-col items-center justify-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-20 animate-pulse"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.182 16.318A4.486 4.486 0 0 0 12.016 15a4.486 4.486 0 0 0-3.198 1.318M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
+              />
+            </svg>
+            <p class="text-3xl animate-pulse">No Following Post Available!!</p>
+          </div>
+        </div>
       </div>
       <!-- ================================= -->
 
@@ -19,4 +70,18 @@
   </div>
 </template>
 
-<script setup lang=""></script>
+<script setup lang="">
+  const userStore = useUserStore();
+  const activeShowAllPosts = ref(true);
+  const activeShowFollowedPosts = ref(false);
+
+  const ShowAllPosts = () => {
+    activeShowFollowedPosts.value = false;
+    activeShowAllPosts.value = true;
+  };
+
+  const ShowFollowedPosts = () => {
+    activeShowAllPosts.value = false;
+    activeShowFollowedPosts.value = true;
+  };
+</script>
